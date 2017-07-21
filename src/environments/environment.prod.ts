1695251ae0820684f03acd4bcdaf64aa62604381
Luciana Lima <lucianalima.lml@gmail.com>
2017-07-21 17:07:03 -0300

    enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:8090/rest/people'
+  api_url: './assets/people.json'
+  // api_url: 'http://localhost:8090/rest/people'
 };
